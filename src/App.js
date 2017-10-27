7903e13c0b96b720300dfb515c30f2c32857b2d0
ARUN KUMAR <arunpasupathi24@gmail.com>
2017-10-27 07:45:19 +0530

    Firebase added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
 import React, { Component } from 'react';
 import Section from './layout/Section';
+import firebase from 'firebase';
 
 class App extends Component {
+
+  constructor(){
+    super();
+
+    this.db = firebase.initializeApp({
+      apiKey: "AIzaSyAZPIarB-HRUBT2VLHROL-g6dXFngKXLUU",
+      authDomain: "authentication-8d1c1.firebaseapp.com",
+      databaseURL: "https://authentication-8d1c1.firebaseio.com",
+      projectId: "authentication-8d1c1",
+      storageBucket: "authentication-8d1c1.appspot.com",
+      messagingSenderId: "553904681564"
+    });
+    this.data = this.db.database().ref().child("Arun");
+    console.log(this.data)
+  }
+
   render() {
     const { mainStyle } = Styles;
     return (
